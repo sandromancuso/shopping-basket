4602b5753980d214c62cc4ed2fc1795e2d2d267a
Sandro Mancuso <sandromancuso@yahoo.com>
2016-07-11 21:41:07 +0100

    Exercise 10: Changed description of Scenario 1 after feedback from apprentices

--- ShoppingCartServiceShould.java
+++ ShoppingCartServiceShould.java
@@ -18,9 +18,10 @@ public class ShoppingCartServiceShould {
     private static final UserId USER_ID = new UserId("U1111");
     private static final ProductId PRODUCT_ID = new ProductId("P0012");
     private static final int QUANTITY = 10;
+    public static final int QUANTITY_10 = 10;
 
     @Mock ShoppingCartRepository shoppingCartRepository;
-    @Mock ProductService productService;
+    @Mock PriceService priceService;
     @Mock Clock clock;
     @Mock ShoppingCartLogger logger;
 
@@ -31,14 +32,14 @@ public class ShoppingCartServiceShould {
         shoppingCartService = new ShoppingCartService(
                 clock,
                 logger,
-                productService,
+                priceService,
                 shoppingCartRepository);
     }
 
     @Test public void
     add_items_to_shopping_cart() {
         ShoppingCartItem shoppingCartItem = new ShoppingCartItem(USER_ID, PRODUCT_ID, QUANTITY);
-        given(productService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
+        given(priceService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
 
         shoppingCartService.addProduct(USER_ID, PRODUCT_ID, QUANTITY);
 
@@ -47,10 +48,10 @@ public class ShoppingCartServiceShould {
 
     @Test public void
     log_each_item_added_to_shopping_cart() {
-        given(productService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
+        given(priceService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
         given(clock.dateInYYYYMMDD()).willReturn("2016-07-01");
 
-        shoppingCartService.addProduct(USER_ID, PRODUCT_ID, QUANTITY);
+        shoppingCartService.addProduct(USER_ID, PRODUCT_ID, QUANTITY_10);
 
         verify(logger).log("[ITEM ADDED TO SHOPPING CART]: Date[2016-07-01], User[U1111], Product[P0012], Quantity[10], Total price[100.00]");
     }
