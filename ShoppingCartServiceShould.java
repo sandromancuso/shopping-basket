26a084ca1df268be015fa8e1335fa4d6b8636143
Sandro Mancuso <sandromancuso@yahoo.com>
2016-07-11 15:52:47 +0100

    Exercise 10 - Second test implemented: ShoppingCartServiceShould.log_each_item_added_to_shopping_cart()

--- ShoppingCartServiceShould.java
+++ ShoppingCartServiceShould.java
@@ -1,36 +1,58 @@
 package com.codurance.craftingcode.exercise_10_shopping_cart;
 
+import com.codurance.craftingcode.exercise_05_vanila_bank_kata.Clock;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShoppingCartServiceShould {
 
-    private static final UserId USER_ID = new UserId("some id");
-    private static final ProductId PRODUCT_ID = new ProductId("product id");
+    private static final UserId USER_ID = new UserId("U1111");
+    private static final ProductId PRODUCT_ID = new ProductId("P0012");
     private static final int QUANTITY = 10;
 
     @Mock ShoppingCartRepository shoppingCartRepository;
+    @Mock ProductService productService;
+    @Mock Clock clock;
+    @Mock ShoppingCartLogger logger;
 
     private ShoppingCartService shoppingCartService;
 
     @Before
     public void initialise() {
-        shoppingCartService = new ShoppingCartService(shoppingCartRepository);
+        shoppingCartService = new ShoppingCartService(
+                clock,
+                logger,
+                productService,
+                shoppingCartRepository);
     }
 
     @Test public void
     add_items_to_shopping_cart() {
         ShoppingCartItem shoppingCartItem = new ShoppingCartItem(USER_ID, PRODUCT_ID, QUANTITY);
+        given(productService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
 
         shoppingCartService.addProduct(USER_ID, PRODUCT_ID, QUANTITY);
 
         verify(shoppingCartRepository).add(shoppingCartItem);
+    }
+
+    @Test public void
+    log_each_item_added_to_shopping_cart() {
+        given(productService.priceFor(PRODUCT_ID)).willReturn(BigDecimal.valueOf(10.00));
+        given(clock.dateInYYYYMMDD()).willReturn("2016-07-01");
+
+        shoppingCartService.addProduct(USER_ID, PRODUCT_ID, QUANTITY);
+
+        verify(logger).log("[ITEM ADDED TO SHOPPING CART]: Date[2016-07-01], User[U1111], Product[P0012], Quantity[10], Total price[100.00]");
     }
 
 }
