63e1c54314504a3bf01e4dd2aee340896cc07cac
Sandro Mancuso <sandromancuso@yahoo.com>
2016-07-11 21:41:07 +0100

    Exercise 10: Changed description of Scenario 1 after feedback from apprentices

--- ShoppingCartLogger.java
+++
@@ -1,7 +0,0 @@
-package com.codurance.craftingcode.exercise_10_shopping_cart;
-
-public class ShoppingCartLogger {
-    public void log(String text) {
-        throw new UnsupportedOperationException();
-    }
-}
--- ShoppingCartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codurance.craftingcode.exercise_10_shopping_cart;
-
-public class ShoppingCartRepository {
-    public void add(ShoppingCartItem shoppingCartItem) {
-
-    }
-}
