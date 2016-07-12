a77386c20e465551de425e039c45c9b1357c39af
Sandro Mancuso <sandromancuso@yahoo.com>
2016-07-12 17:33:23 +0100

    Finished first scenario

--- ShoppingBasketService.java
+++ ShoppingBasketService.java
@@ -7,14 +7,16 @@ public class ShoppingBasketService {
     private PriceService priceService;
     private ShoppingBasketRepository shoppingBasketRepository;
 
-    public ShoppingBasketService(PriceService priceService, ShoppingBasketRepository shoppingBasketRepository) {
+    public ShoppingBasketService(PriceService priceService,
+                                 ShoppingBasketRepository shoppingBasketRepository) {
         this.priceService = priceService;
         this.shoppingBasketRepository = shoppingBasketRepository;
     }
 
     public void addItem(UserID userID, ProductID productID, int quantity) {
         BigDecimal productUnitPrice = priceService.priceFor(productID);
-        ShoppingBasketItem item = new ShoppingBasketItem(productID, quantity, productUnitPrice);
+        ShoppingBasketItem item =
+            new ShoppingBasketItem(productID, quantity, productUnitPrice);
         shoppingBasketRepository.addItem(userID, item);
     }
 
