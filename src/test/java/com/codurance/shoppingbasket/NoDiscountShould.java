58df70217f748fc598ace38506deb53369d84e4b
Sandro Mancuso <sandromancuso@yahoo.com>
2016-08-02 01:15:50 +0100

    Tests for NoDiscount

---
+++ src/test/java/com/codurance/shoppingbasket/NoDiscountShould.java
@@ -0,0 +1,31 @@
+package com.codurance.shoppingbasket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.codurance.shoppingbasket.ShoppingBasketBuilder.aShoppingBasket;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class NoDiscountShould {
+
+	private Discount discount;
+
+	@Before
+	public void initialise() {
+	    discount = new NoDiscount();
+	}
+
+	@Test public void
+	return_percentage_of_zero() {
+	    assertThat(discount.percentage(), is(0));
+	}
+
+	@Test public void
+	be_applicable_to_any_shopping_basket() {
+		ShoppingBasket shoppingBasket = aShoppingBasket().build();
+
+		assertThat(discount.isApplicableTo(shoppingBasket), is(true));
+	}
+
+}
